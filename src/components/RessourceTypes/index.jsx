23469e35e4d1b99a42516b287d5193654fec206b
najinj <naji.ensat@gmail.com>
2019-09-25 00:17:34 +0200

    Redux added to Components/RessourceType

--- src/components/RessourceTypes/index.jsx
+++ src/components/RessourceTypes/index.jsx
@@ -1,9 +1,12 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
-import React,{useState} from "react";
-import { Table, Input, InputNumber, Popconfirm, Form } from 'antd';
-import {connect} from "react-redux";
+import React, { useState, useEffect } from "react";
+import { Table, Input, InputNumber, Popconfirm, Form } from "antd";
+import { connect } from "react-redux";
+import {
+  fetchRessourceTypes,
+  updateRessourceType,
+  deleteRessourceType,
+  addRessourceType
+} from "../../actions/ressourceTypes-actions/actions";
 
 import "antd/es/table/style/css";
 import "antd/es/input/style/css";
@@ -17,187 +20,214 @@ for (let i = 0; i < 100; i += 1) {
     key: i.toString(),
     name: `Edrward ${i}`,
     age: 32,
-    address: `London Park no. ${i}`,
+    address: `London Park no. ${i}`
   });
 }
 
-
-
 const EditableContext = React.createContext();
 
-
 const EditableCell = ({
-    editing,
-    dataIndex,
-    title,
-    inputType,
-    record,
-    index,
-    children,
-    ...restProps
-  }) =>{
-    const getInput = (type) => {
-        if (type === 'number') {
-          return <InputNumber />;
-        }
-        return <Input />;
-      };
-    const renderCell = ({ getFieldDecorator }) => {
-        return (
-          <td {...restProps}>
-            {editing ? (
-              <Form.Item style={{ margin: 0 }}>
-                {getFieldDecorator(dataIndex, {
-                  rules: [
-                    {
-                      required: true,
-                      message: `Please Input ${title}!`,
-                    },
-                  ],
-                  initialValue: record[dataIndex],
-                })(getInput(inputType))}
-              </Form.Item>
-            ) : (
-              children
-            )}
-          </td>
-        );
-      };
-    return <EditableContext.Consumer>{renderCell}</EditableContext.Consumer>;  
-}
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const getInput = type => {
+    if (type === "number") {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+  const renderCell = ({ getFieldDecorator }) => {
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator(dataIndex, {
+              rules: [
+                {
+                  required: true,
+                  message: `Please Input ${title}!`
+                }
+              ],
+              initialValue: record[dataIndex]
+            })(getInput(inputType))}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+  return <EditableContext.Consumer>{renderCell}</EditableContext.Consumer>;
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchRessourceTypes: () => dispatch(fetchRessourceTypes()),
+    updateRessourceType: (id, ressourceType) =>
+      dispatch(updateRessourceType(id, ressourceType)),
+    deleteRessourceType: id => dispatch(deleteRessourceType(id)),
+    addRessourceType: ressourceType => dispatch(addRessourceType(ressourceType))
+  };
+};
 
+const mapStateToProps = state => {
+  return {
+    ressourceTypes: state.ressourceTypeReducer.ressourceTypes,
+    isLoading: state.ressourceTypeReducer.isLoading
+  };
+};
+const EditableTable = ({
+  form,
+  ressourceTypes,
+  isLoading,
+  fetchRessourceTypes,
+  updateRessourceType,
+  deleteRessourceType,
+  addRessourceType
+}) => {
+  const [editingKey, SetEditingKey] = useState("");
 
-const EditableTable = ({form}) =>{
+  useEffect(() => fetchRessourceTypes(), []);
 
-    const [myData,SetMyData] = useState(data);
-    const [editingKey,SetEditingKey] = useState("");
+  const isEditing = record => record.key === editingKey;
 
-    const isEditing = record => record.key === editingKey;
+  const cancel = () => {
+    SetEditingKey("");
+  };
 
-    const cancel = () => {
-        SetEditingKey("");
-      };
-    
-    const save = (formIn, key) => {
-        formIn.validateFields((error, row) => {
-          if (error) {
-            return;
-          }
-          const newData = [...myData];
-          const index = newData.findIndex(item => key === item.key);
-          if (index > -1) {
-            const item = newData[index];
-            newData.splice(index, 1, {
-              ...item,
-              ...row,
-            });
-            SetMyData(newData);
-            SetEditingKey("");
-          } else {
-            newData.push(row);
-            SetEditingKey("");
-            SetMyData(newData);
-          }
-        });
+  const save = (formIn, key) => {
+    formIn.validateFields((error, row) => {
+      if (error) {
+        return;
       }
-    
-    const edit =(key) => {
-        SetEditingKey(key);
-      }  
 
-    const columns = [
-        {
-          title: 'name',
-          dataIndex: 'name',
-          width: '25%',
-          editable: true,
-        },
-        {
-          title: 'age',
-          dataIndex: 'age',
-          width: '15%',
-          editable: true,
-        },
-        {
-          title: 'address',
-          dataIndex: 'address',
-          width: '40%',
-          editable: true,
-        },
-        {
-          title: 'operation',
-          dataIndex: 'operation',
-          render: (text, record) => {
-            const editable = isEditing(record);
-            return editable ? (
-              <span>
-                <EditableContext.Consumer>
-                  {myForm => (
-                    <a
-                      role="presentation"
-                      onKeyPress={()=>{}}
-                      onClick={() => save(myForm, record.key)}
-                      style={{ marginRight: 8 }}
-                    >
-                      Save
-                    </a>
-                  )}
-                </EditableContext.Consumer>
-                <Popconfirm title="Sure to cancel?" onKeyPress={()=>{}} onConfirm={() => cancel(record.key)}>
-                  <a>Cancel</a>
-                </Popconfirm>
-              </span>
-            ) : (
-              <a role="presentation" disabled={editingKey !== ''} onKeyPress={()=>{}} onClick={() => edit(record.key)}>
-                Edit
-              </a>
-            );
-          },
-        },
-      ];
+      const index = ressourceTypes.findIndex(item => key === item.key);
+      if (index > -1) {
+        updateRessourceType(row.key, row);
+      } else {
+        addRessourceType(row);
+      }
+    });
+  };
 
-      const components = {
-        body: {
-          cell: EditableCell,
-        },
-      };
-  
-      const columnsMaped = columns.map(col => {
-        if (!col.editable) {
-          return col;
-        }
-        return {
-          ...col,
-          onCell: record => ({
-            record,
-            inputType: col.dataIndex === 'age' ? 'number' : 'text',
-            dataIndex: col.dataIndex,
-            title: col.title,
-            editing: isEditing(record),
-          }),
-        };
-      });  
-      return (
-        <EditableContext.Provider value={form}>
-          <Table
-            components={components}
-            bordered
-            dataSource={myData}
-            columns={columnsMaped}
-            rowClassName="editable-row"
-            pagination={{
-              onChange: cancel,
-            }}
-          />
-        </EditableContext.Provider>
-      );  
-    
-}
+  const edit = key => {
+    SetEditingKey(key);
+  };
 
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      width: "25%",
+      editable: true
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      width: "15%",
+      editable: true
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      width: "40%",
+      editable: true
+    },
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (text, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <EditableContext.Consumer>
+              {myForm => (
+                <a
+                  role="presentation"
+                  onKeyPress={() => {}}
+                  onClick={() => save(myForm, record.key)}
+                  style={{ marginRight: 8 }}
+                >
+                  Save
+                </a>
+              )}
+            </EditableContext.Consumer>
+            <Popconfirm
+              title="Sure to cancel?"
+              onKeyPress={() => {}}
+              onConfirm={() => cancel(record.key)}
+            >
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <a
+            role="presentation"
+            disabled={editingKey !== ""}
+            onKeyPress={() => {}}
+            onClick={() => edit(record.key)}
+          >
+            Edit
+          </a>
+        );
+      }
+    }
+  ];
 
+  const components = {
+    body: {
+      cell: EditableCell
+    }
+  };
+
+  const MappedRessourceTypes = ressourceTypes.map(ressourceType => ({
+    key: ressourceType.Id,
+    name: ressourceType.name,
+    description: ressourceType.ressourceType,
+    type: ressourceType.type
+  }));
+  const columnsMaped = columns.map(col => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        inputType: col.dataIndex === "age" ? "number" : "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record)
+      })
+    };
+  });
+  return (
+    <EditableContext.Provider value={form}>
+      <Table
+        components={components}
+        bordered
+        dataSource={MappedRessourceTypes}
+        columns={columnsMaped}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel
+        }}
+        loading={isLoading}
+      />
+    </EditableContext.Provider>
+  );
+};
 
 const EditableFormTable = Form.create()(EditableTable);
 
-const ConnectedEditableFormTable = connect()(EditableFormTable);
+const ConnectedEditableFormTable = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditableFormTable);
 
 export default ConnectedEditableFormTable;
--- src/reducers/ressourceTypeReducer.js
+++ src/reducers/ressourceTypeReducer.js
@@ -1,71 +1,78 @@
 /* eslint-disable no-unused-vars */
 import {
-    ADD_RESSOURCE_TYPE_FAILURE,
-    ADD_RESSOURCE_TYPE_REQUEST,
-    ADD_RESSOURCE_TYPE_SUCCESS,
-    DELETE_RESSOURCE_TYPE_FAILURE,
-    DELETE_RESSOURCE_TYPE_REQUEST,
-    DELETE_RESSOURCE_TYPE_SUCCESS,
-    FETCH_RESSOURCE_TYPES_FAILURE,
-    FETCH_RESSOURCE_TYPES_REQUEST,
-    FETCH_RESSOURCE_TYPES_SUCCESS,
-    UPDATE_RESSOURCE_TYPE_FAILURE,
-    UPDATE_RESSOURCE_TYPE_REQUEST,
-    UPDATE_RESSOURCE_TYPE_SUCCESS
+  ADD_RESSOURCE_TYPE_FAILURE,
+  ADD_RESSOURCE_TYPE_REQUEST,
+  ADD_RESSOURCE_TYPE_SUCCESS,
+  DELETE_RESSOURCE_TYPE_FAILURE,
+  DELETE_RESSOURCE_TYPE_REQUEST,
+  DELETE_RESSOURCE_TYPE_SUCCESS,
+  FETCH_RESSOURCE_TYPES_FAILURE,
+  FETCH_RESSOURCE_TYPES_REQUEST,
+  FETCH_RESSOURCE_TYPES_SUCCESS,
+  UPDATE_RESSOURCE_TYPE_FAILURE,
+  UPDATE_RESSOURCE_TYPE_REQUEST,
+  UPDATE_RESSOURCE_TYPE_SUCCESS
 } from "../actions/ressourceTypes-actions/types";
 
 const intialState = {
-    ressourceTypes: []
-  };
+  ressourceTypes: [],
+  isLoading
+};
 
+export default function ressourceTypeReducer(state = intialState, action) {
+  switch (action.type) {
+    case FETCH_RESSOURCE_TYPES_REQUEST:
+      return { ...state, isLoading: true };
 
-  export default function ressourceTypeReducer(state = intialState,action){
-      switch(action.type){
-        case FETCH_RESSOURCE_TYPES_REQUEST:
-            return state;
-      
-        case FETCH_RESSOURCE_TYPES_SUCCESS:
-            return {
-              ...state,
-              ressourceTypes : action.payload
-            };
-        case FETCH_RESSOURCE_TYPES_FAILURE:
-            return state; 
-        /* *************************** */
-        case ADD_RESSOURCE_TYPE_REQUEST:
-            return state;
-      
-        case ADD_RESSOURCE_TYPE_SUCCESS:
-            return {
-              ...state,
-              ressourceTypes : [...state.ressourceTypes,action.payload]
-            };
-        case ADD_RESSOURCE_TYPE_FAILURE:
-            return state; 
-        /* *************************** */    
-        case UPDATE_RESSOURCE_TYPE_REQUEST:
-                return state;                  
-        case UPDATE_RESSOURCE_TYPE_SUCCESS:
-            return {
-              ...state,
-              ressourceTypes : state.ressourceTypes.map(ressourceType=>{
-                  if(ressourceType.Id)
-                    return  action.payload;
-                  return ressourceType;  
-              })
-            };
-        case UPDATE_RESSOURCE_TYPE_FAILURE:
-            return state; 
-        /* *************************** */ 
-        case DELETE_RESSOURCE_TYPE_REQUEST:
-            return state; 
+    case FETCH_RESSOURCE_TYPES_SUCCESS:
+      return {
+        ...state,
+        ressourceTypes: action.payload,
+        isLoading: false
+      };
+    case FETCH_RESSOURCE_TYPES_FAILURE:
+      return { ...state, isLoading: true };
+    /* *************************** */
+    case ADD_RESSOURCE_TYPE_REQUEST:
+      return state;
 
-        case DELETE_RESSOURCE_TYPE_SUCCESS:
-                return {...state ,ressourceTypes : state.ressourceTypes.filter(ressourceType => ressourceType.id !== action.payload.id )};
-      
-        case DELETE_RESSOURCE_TYPE_FAILURE:
-                return state; 
-          default :
-                return state;
-      }
+    case ADD_RESSOURCE_TYPE_SUCCESS:
+      return {
+        ...state,
+        ressourceTypes: [...state.ressourceTypes, action.payload]
+      };
+    case ADD_RESSOURCE_TYPE_FAILURE:
+      return state;
+    /* *************************** */
+
+    case UPDATE_RESSOURCE_TYPE_REQUEST:
+      return state;
+    case UPDATE_RESSOURCE_TYPE_SUCCESS:
+      return {
+        ...state,
+        ressourceTypes: state.ressourceTypes.map(ressourceType => {
+          if (ressourceType.Id) return action.payload;
+          return ressourceType;
+        })
+      };
+    case UPDATE_RESSOURCE_TYPE_FAILURE:
+      return state;
+    /* *************************** */
+
+    case DELETE_RESSOURCE_TYPE_REQUEST:
+      return state;
+
+    case DELETE_RESSOURCE_TYPE_SUCCESS:
+      return {
+        ...state,
+        ressourceTypes: state.ressourceTypes.filter(
+          ressourceType => ressourceType.id !== action.payload.id
+        )
+      };
+
+    case DELETE_RESSOURCE_TYPE_FAILURE:
+      return state;
+    default:
+      return state;
   }
+}
