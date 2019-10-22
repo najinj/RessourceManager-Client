c9c14599dcdf69c7126f946444f3b78dc0a645d5
Naji Naji <nnaji@sqli.com>
2019-10-22 14:12:06 +0100

    Connect App component to redux ,passing loggin Info to routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,48 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import {connect} from "react-redux";
 import WrappedSignInForm from "./SignInForm";
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 import LoginForm from "./LoginForm";
 import SideNav from "./HomePage";
-import store from "../store/index";
-import { history } from "../config";
 
-export default () => (
-  <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/Signup" component={WrappedSignInForm} />
-        <Route exact path="/login" component={LoginForm} />
-        <PrivateRoute path="/" component={SideNav} />
-      </Switch>
-    </Router>
-  </Provider>
+
+const mapStateToProps = reduxStore => {
+  return {
+    isLoggedIn: reduxStore.authReducer.isLoggedIn,
+    email: reduxStore.authReducer.email,
+    isLoadingUser: reduxStore.authReducer.isLoadingUser
+  };
+};
+
+const App = () => (
+
+  <Router history={props.history}>
+  <Switch>
+    <PublicRoute 
+        exact path="/Signup" 
+        component={WrappedSignInForm}
+        email={props.email}
+        authenticated={props.isLoggedIn}/>
+    <PublicRoute exact path="/login" 
+        component={LoginForm}
+        email={props.email}
+        authenticated={props.isLoggedIn} />
+    <PrivateRoute 
+        path="/" 
+        component={SideNav}
+        email={props.email}
+        authenticated={props.isLoggedIn}/>
+  </Switch>
+</Router>
 );
+const ConnectedApp = connect(
+  mapStateToProps,
+  null
+)(App);
+
+export default ConnectedApp;
