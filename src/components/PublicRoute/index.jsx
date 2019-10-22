1e979190754865480cea67cd0ef0d6cc2f79df84
Naji Naji <nnaji@sqli.com>
2019-10-22 14:25:52 +0100

    Add PublicRoute component

---
+++ src/components/PublicRoute/index.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { func } from "prop-types";
+
+// eslint-disable-next-line react/prop-types
+const PublicRoute = ({ component : Component,authenticated, ...options }) => {
+  console.log("PublicRoute authenticated",authenticated)
+return(
+  <Route
+        {...options}
+        exact
+        render={props =>
+          !authenticated ? <Component {...props} /> : <Redirect to="/" />
+        }
+      />
+)
+  
+};
+
+PublicRoute.propTypes = {
+  component: func
+};
+PublicRoute.defaultProps = {
+  component: null
+};
+export default PublicRoute;
