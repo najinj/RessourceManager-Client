2cc4cfda3389619ae83a7fe50cf4b97a6ae94565
najinj <naji.ensat@gmail.com>
2019-12-07 18:44:11 +0100

    Add logic to retrive reservation using Resource select component -add antd side calendar to select week and connect it to full weekly calendar view -Replace time fromat from 12h to 24h

--- src/components/calendar/index.jsx
+++ src/components/calendar/index.jsx
@@ -6,6 +6,7 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import FullCalendar from "@fullcalendar/react";
+import { Calendar, Select } from "antd";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
@@ -13,11 +14,15 @@ import {
   getReservationsByResource,
   addReservations
 } from "../../actions/reservation-action/action";
+import { fetchSpaces } from "../../actions/space-actions/actions";
+
+import "./index.css";
 
 const mapStateToProps = state => {
   return {
     reservations: state.reservationReducer.calendarState.reservations,
-    resourceId: state.reservationReducer.calendarState.resourceId
+    resourceId: state.reservationReducer.calendarState.resourceId,
+    spaces: state.spaceReducer.spaces
   };
 };
 
@@ -25,15 +30,19 @@ const mapDispatchToProps = dispatch => {
   return {
     loadReservations: resourceId =>
       dispatch(getReservationsByResource(resourceId)),
-    addReservation: reservation => dispatch(addReservations(reservation))
+    addReservation: reservation => dispatch(addReservations(reservation)),
+    loadSpaces: () => dispatch(fetchSpaces())
   };
 };
+const { Option } = Select;
 
-const Calendar = ({
+const CalendarView = ({
   reservations,
   resourceId,
   loadReservations,
-  addReservation
+  addReservation,
+  loadSpaces,
+  spaces
 }) => {
   // eslint-disable-next-line no-unused-vars
   const [calendarWeekends, SetCalendarWeekends] = useState(true);
@@ -43,8 +52,15 @@ const Calendar = ({
   const calendarComponentRef = React.createRef();
 
   useEffect(() => {
-    loadReservations("5dbb45b226c6e640dc563ba0");
+    loadSpaces();
   }, []);
+
+  const spaceOptions = spaces.map(space => {
+    return {
+      text: space.name,
+      value: space.id
+    };
+  });
 
   /*
   const toggleWeekends = () => {
@@ -53,8 +69,7 @@ const Calendar = ({
 
  
   const gotoPast = () => {
-    const calendarApi = calendarComponentRef.current.getApi();
-    calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
+    
   };
 
 */
@@ -89,19 +104,45 @@ const Calendar = ({
     alert("Event: " + info.event.title);
     alert("Coordinates: " + info.jsEvent.pageX + "," + info.jsEvent.pageY);
     alert("View: " + info.view.type);
+  };
+  const onPanelChange = (value, mode) => {
+    const calendarApi = calendarComponentRef.current.getApi();
+    calendarApi.gotoDate(value.format("YYYY-MM-DD")); // call a method on the Calendar object
+  };
 
-    // change the border color just for fun
+  const hadnleSpaceChange = value => {
+    loadReservations(value);
   };
 
   return (
-    <div className="demo-app">
+    <div className="calendar-container">
+      <div className="select-container">
+        <div className="space-select-container">
+          <Select
+            initialValue=""
+            style={{ width: 120 }}
+            onChange={hadnleSpaceChange}
+            placeholder="Select a space"
+          >
+            {spaceOptions.map(option => (
+              <Option value={option.value} key={option.value}>
+                {option.text}
+              </Option>
+            ))}
+          </Select>
+        </div>
+        <div className="monthly-sideview-calendar">
+          <Calendar fullscreen={false} onChange={onPanelChange} />
+        </div>
+      </div>
+
       <div className="demo-app-calendar">
         <FullCalendar
           defaultView="timeGridWeek"
           header={{
             left: "prev,next today",
             center: "title",
-            right: "timeGridWeek"
+            right: null
           }}
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           ref={calendarComponentRef}
@@ -112,10 +153,24 @@ const Calendar = ({
           allDaySlot={false}
           minTime="08:00:00"
           maxTime="18:00:00"
-          height="parent"
+          height="auto"
           firstDay={1}
           eventClick={eventClick}
-        />
+          slotLabelFormat={{
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false
+          }}
+        >
+          <div className="fc-right">
+            <button
+              type="button"
+              className="fc-timeGridWeek-button fc-button fc-button-primary fc-button-active"
+            >
+              week
+            </button>
+          </div>
+        </FullCalendar>
       </div>
     </div>
   );
@@ -124,6 +179,6 @@ const Calendar = ({
 const ConnectedCalendar = connect(
   mapStateToProps,
   mapDispatchToProps
-)(Calendar);
+)(CalendarView);
 
 export default ConnectedCalendar;
