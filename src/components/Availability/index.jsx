a91a84d9e96b783fd9c0b012b9082b6388dfc7ae
najinj <naji.ensat@gmail.com>
2019-12-17 22:38:18 +0100

    Remove Logic for searching for availability

--- src/components/Availability/index.jsx
+++ src/components/Availability/index.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { List } from "antd";
 import AvailabilityForm from "../AvailabilityForm";
 import Reservation from "../Reservation";
-
-import { getAvailability } from "../../actions/reservation-action/action";
 
 const resourceTypes = [
   {
@@ -19,48 +17,19 @@ const resourceTypes = [
   }
 ];
 
-const Availability = ({ resources, resourceType, checkAvailability }) => {
-  const [availabilityFilter, SetAvailabilityFilter] = useState(null);
-  const getCronosExpression = (date, days) => {
-    return `${date.format("mm")} ${date.format("HH")} * * ${
-      days.length === 7 ? "*" : days.toString()
-    }`;
-  };
-  const search = values => {
-    const reservation = { ...values };
-    if (Array.isArray(values.weekDays) && values.weekDays.length) {
-      reservation.CronosExpression = getCronosExpression(
-        values.startTime,
-        values.weekDays
-      );
-    }
-    reservation.start.utcOffset(0);
-    reservation.end.utcOffset(0);
-    reservation.start.set({
-      hour: reservation.startTime.hours(),
-      minute: reservation.startTime.minutes(),
-      second: 0,
-      millisecond: 0
-    });
-    reservation.end.set({
-      hour: reservation.endTime.hours(),
-      minute: reservation.endTime.minutes(),
-      second: 0,
-      millisecond: 0
-    });
-    SetAvailabilityFilter(reservation);
-    checkAvailability(reservation);
-  };
+const Availability = ({ resources, availabilityForm }) => {
   const mappedReservations = resources.map(resource => {
     return {
       ...resource,
-      start: availabilityFilter.start,
-      end: availabilityFilter.end
+      start: availabilityForm.start,
+      end: availabilityForm.end,
+      resourceType: availabilityForm.resourceType
     };
   });
+
   return (
     <div className="availability-container">
-      <AvailabilityForm search={search} resourceTypes={resourceTypes} />
+      <AvailabilityForm resourceTypes={resourceTypes} />
       <List
         itemLayout="vertical"
         size="sm"
@@ -80,22 +49,17 @@ const Availability = ({ resources, resourceType, checkAvailability }) => {
     </div>
   );
 };
-const mapDispachToProps = dispatch => {
-  return {
-    checkAvailability: criteria => dispatch(getAvailability(criteria))
-  };
-};
 
 const mapStateToProps = state => {
   return {
     resources: state.reservationReducer.availability.resources,
-    resourceType: state.reservationReducer.availability.resourceType
+    availabilityForm: state.reservationReducer.availability.availabilityForm
   };
 };
 
 const ConnectedAvailability = connect(
   mapStateToProps,
-  mapDispachToProps
+  null
 )(Availability);
 
 export default ConnectedAvailability;
