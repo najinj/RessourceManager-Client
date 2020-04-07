f502e8d33ea5b2cf605993854371520b1e154035
nnaji <nnaji@sqli.com>
2020-04-07 20:25:55 +0100

    Call ModalForm on Add Reservation and pass reservation info to the Modal

--- src/components/Reservation/index.jsx
+++ src/components/Reservation/index.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import { Card, Button, Icon, Popover, Tag, Modal } from "antd";
+import { Card, Button, Icon, Popover, Tag, Modal, Form } from "antd";
 import QueueAnim from "rc-queue-anim";
 import moment from "moment";
 import { connect } from "react-redux";
@@ -8,18 +10,67 @@ import { shape, func, arrayOf, bool, number, string } from "prop-types";
 import {
   deletePeriodicReservations,
   deleteReservation,
-  addReservations
+  addReservations,
+  fillReservationForm,
+  setUserAction,
+  emptyReservationForm
 } from "../../actions/reservation-action/action";
 
 import "./index.css";
 
 const { confirm } = Modal;
-
+const columns = [
+  {
+    title: "Resource",
+    dataIndex: "resourceName",
+    required: true,
+    editable: false,
+    inputType: "readOnly"
+  },
+  {
+    title: "From",
+    dataIndex: "start",
+    editable: false,
+    required: true,
+    inputType: "readOnly"
+  },
+  {
+    title: "To",
+    dataIndex: "end",
+    editable: false,
+    required: true,
+    inputType: "readOnly"
+  },
+  {
+    title: "Description",
+    dataIndex: "title",
+    editable: true,
+    required: false,
+    inputType: "text"
+  },
+  {
+    title: "Resource Type",
+    dataIndex: "resourceType",
+    editable: false,
+    hidden: true,
+    inputType: "hidden"
+  },
+  {
+    title: "Resource",
+    dataIndex: "resourceId",
+    editable: false,
+    hidden: true,
+    inputType: "hidden"
+  }
+];
 const mapDispatchToProps = dispatch => {
   return {
     removeReservation: id => dispatch(deleteReservation(id)),
     removePeriodicReservation: id => dispatch(deletePeriodicReservations(id)),
-    addReservation: reservation => dispatch(addReservations(reservation))
+    addEntitie: reservation => dispatch(addReservations(reservation)),
+    openForm: form => dispatch(fillReservationForm(form)),
+    closeForm: () => dispatch(emptyReservationForm()),
+    setUserAction: action => dispatch(setUserAction(action))
   };
 };
 
@@ -29,14 +80,18 @@ const Reservation = ({
   removeReservation,
   removePeriodicReservation,
   isAvailability,
-  addReservation
+  // eslint-disable-next-line no-unused-vars
+  addEntitie,
+  openForm,
+  // eslint-disable-next-line no-shadow
+  setUserAction
 }) => {
-  console.log("isAvailability", isAvailability);
   const [showPeriodicReservation, SetPeriodicReservation] = useState(false);
 
   useEffect(() => {
     SetPeriodicReservation(false);
   }, [reservation]);
+
   const resourceName =
     reservation.name !== undefined
       ? reservation.name
@@ -61,6 +116,7 @@ const Reservation = ({
   const show = () => {
     SetPeriodicReservation(!showPeriodicReservation);
   };
+
   const getWeekDaysNames = indexes => {
     const defaultWeekdays = moment.weekdays();
     console.log(defaultWeekdays);
@@ -97,20 +153,50 @@ const Reservation = ({
     });
   };
 
-  const bookReservation = reservationIn => {
-    const reservationModel = {
+  const bookReservation = e => {
+    const formColumns = columns.map(col => {
+      return {
+        ...col,
+        onCell: record => ({
+          key: `_${col.dataIndex}`,
+          record,
+          required: col.required,
+          inputType: col.inputType,
+          dataIndex: col.dataIndex,
+          title: col.title
+        })
+      };
+    });
+    const record = {
       resourceName,
-      start: reservationIn.start,
-      end: reservationIn.end,
-      title: reservationIn.title,
-      resourceType: reservationIn.resourceType,
-      resourceId: reservationIn.id
+      start: {
+        value: reservation.start.format(),
+        text: `${moment(reservation.start).format("DD/MM/YYYY")} at ${moment(
+          reservation.start
+        ).format("HH:mm")}`
+      },
+      end: {
+        value: reservation.end.format(),
+        text: `${moment(reservation.end).format("DD/MM/YYYY")} at ${moment(
+          reservation.end
+        ).format("HH:mm")}`
+      },
+      title: "",
+      resourceType: reservation.resourceType,
+      resourceId: reservation.id
     };
-    addReservation(reservationModel);
+    const fields = formColumns.map(col => col.onCell(record));
+    openForm(fields);
+    setUserAction({ execute: addEntitie });
   };
+
   return (
     <div className="reservation-container">
-      <Card bordered style={{ borderLeft: "1px solid #1890ff" }}>
+      <Card
+        bordered
+        style={{ borderLeft: "1px solid #1890ff" }}
+        key={reservation.id}
+      >
         <div className="icons-container">
           {reservation.periodicId !== undefined &&
           reservation.periodicId !== "" ? (
@@ -162,7 +248,7 @@ const Reservation = ({
         </div>
         <div className="reservation-actions ant-col-4">
           {isAvailability ? (
-            <Button type="primiry" onClick={() => bookReservation(reservation)}>
+            <Button type="button" onClick={bookReservation}>
               Add
             </Button>
           ) : (
@@ -175,81 +261,65 @@ const Reservation = ({
           )}
         </div>
       </Card>
-      <QueueAnim
-        className="parent-reservation"
-        animConfig={[
-          { opacity: [1, 0], translateY: [0, 50] },
-          { opacity: [1, 0], translateY: [0, -50] }
-        ]}
-      >
-        {showPeriodicReservation
-          ? [
-              <Card
-                bordered
-                style={{ borderLeft: "1px solid #1890ff" }}
-                size="small"
-              >
-                <div className="icons-container">
-                  <Popover content="Parent reservation" trigger="hover">
-                    <Icon type="calendar" style={{ fontSize: "20px" }} />
-                    <Icon
-                      type="sync"
-                      style={{ fontSize: "14px" }}
-                      rotate={60}
-                    />
-                  </Popover>
-                  <div style={{ width: 32 }} />
+
+      {showPeriodicReservation
+        ? [
+            <Card
+              bordered
+              style={{ borderLeft: "1px solid #1890ff" }}
+              size="small"
+            >
+              <div className="icons-container">
+                <Popover content="Parent reservation" trigger="hover">
+                  <Icon type="calendar" style={{ fontSize: "20px" }} />
+                  <Icon type="sync" style={{ fontSize: "14px" }} rotate={60} />
+                </Popover>
+                <div style={{ width: 32 }} />
+              </div>
+              <div className="reservation-info ant-col-19">
+                <div className="reservation-header">
+                  <span>Recurrency</span>
+                  <h4>
+                    {getWeekDaysNames([1, 2, 3, 4, 5, 6, 0]).map(day => {
+                      console.log(day);
+                      return <Tag key={day}>{day}</Tag>;
+                    })}
+                  </h4>
                 </div>
-                <div className="reservation-info ant-col-19">
-                  <div className="reservation-header">
-                    <span>Recurrency</span>
+                <div className="reservation-body">
+                  <div className="resourceType ant-col-5">
+                    <span>Start Date</span>
                     <h4>
-                      {getWeekDaysNames([1, 2, 3, 4, 5, 6, 0]).map(day => {
-                        console.log(day);
-                        return <Tag key={day}>{day}</Tag>;
-                      })}
+                      {moment(reservation.startRecur).format("DD/MM/YYYY")}
                     </h4>
                   </div>
-                  <div className="reservation-body">
-                    <div className="resourceType ant-col-5">
-                      <span>Start Date</span>
-                      <h4>
-                        {moment(reservation.startRecur).format("DD/MM/YYYY")}
-                      </h4>
-                    </div>
-                    <div className="resource-date ant-col-4">
-                      <span>End Date</span>
-                      <h4>
-                        {moment(reservation.endRecur).format("DD/MM/YYYY")}
-                      </h4>
-                    </div>
-                    <div className="resource-time ant-col-4">
-                      <span>From</span>
-                      <h4>{moment(reservation.start).format("HH:mm")}</h4>
-                    </div>
-                    <div className="resource-time ant-col-6">
-                      <span>To</span>
-                      <h4>{moment(reservation.end).format("HH:mm")}</h4>
-                    </div>
+                  <div className="resource-date ant-col-4">
+                    <span>End Date</span>
+                    <h4>{moment(reservation.endRecur).format("DD/MM/YYYY")}</h4>
+                  </div>
+                  <div className="resource-time ant-col-4">
+                    <span>From</span>
+                    <h4>{moment(reservation.start).format("HH:mm")}</h4>
+                  </div>
+                  <div className="resource-time ant-col-6">
+                    <span>To</span>
+                    <h4>{moment(reservation.end).format("HH:mm")}</h4>
                   </div>
                 </div>
-                <div className="reservation-actions ant-col-4">
-                  <Popover
-                    content="Cancel Perdoidic Reservation"
-                    trigger="hover"
+              </div>
+              <div className="reservation-actions ant-col-4">
+                <Popover content="Cancel Perdoidic Reservation" trigger="hover">
+                  <Button
+                    type="danger"
+                    onClick={() => cancelPeriodicReservation(reservation)}
                   >
-                    <Button
-                      type="danger"
-                      onClick={() => cancelPeriodicReservation(reservation)}
-                    >
-                      Cancel
-                    </Button>
-                  </Popover>
-                </div>
-              </Card>
-            ]
-          : null}
-      </QueueAnim>
+                    Cancel
+                  </Button>
+                </Popover>
+              </div>
+            </Card>
+          ]
+        : null}
     </div>
   );
 };
@@ -279,7 +349,9 @@ Reservation.propTypes = {
   removeReservation: func,
   removePeriodicReservation: func,
   isAvailability: bool,
-  addReservation: func
+  addEntitie: func,
+  openForm: func,
+  setUserAction: func
 };
 Reservation.defaultProps = {
   reservation: null,
@@ -287,8 +359,11 @@ Reservation.defaultProps = {
   removeReservation: func,
   removePeriodicReservation: func,
   isAvailability: bool,
-  addReservation: func
+  addEntitie: func,
+  openForm: func,
+  setUserAction: func
 };
+
 const ConnectedTeservation = connect(
   null,
   mapDispatchToProps
