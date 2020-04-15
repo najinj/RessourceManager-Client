7a821d6691ba9ad0c8e54a8e480b41147f7242c5
nnaji <nnaji@sqli.com>
2020-04-15 21:57:41 +0100

    Add updateBackOfficeSettings Action creator method

--- src/actions/settings-actions/actions.js
+++ src/actions/settings-actions/actions.js
@@ -1,11 +1,14 @@
 import {
   GET_BACK_OFFICE_SETTINGS_REQUEST,
   GET_BACK_OFFICE_SETTINGS_FAILURE,
-  GET_BACK_OFFICE_SETTINGS_SUCCESS
+  GET_BACK_OFFICE_SETTINGS_SUCCESS,
+  UPDATE_BACK_OFFICE_SETTINGS_REQUEST,
+  UPDATE_BACK_OFFICE_SETTINGS_SUCCESS,
+  UPDATE_BACK_OFFICE_SETTINGS_FAILURE
 } from "./types";
 import SettingsServices from "./service";
 
-export default function getBackOfficeSettings() {
+export function getBackOfficeSettings() {
   return dispatch => {
     dispatch({ type: GET_BACK_OFFICE_SETTINGS_REQUEST });
     SettingsServices.getSettings.then(
@@ -18,6 +21,23 @@ export default function getBackOfficeSettings() {
       err => {
         console.log(GET_BACK_OFFICE_SETTINGS_FAILURE, err);
         dispatch({ type: GET_BACK_OFFICE_SETTINGS_FAILURE });
+      }
+    );
+  };
+}
+export function updateBackOfficeSettings(settings) {
+  return dispatch => {
+    dispatch({ type: UPDATE_BACK_OFFICE_SETTINGS_REQUEST });
+    SettingsServices.updateSettings(settings).then(
+      response => {
+        dispatch({
+          type: UPDATE_BACK_OFFICE_SETTINGS_SUCCESS,
+          payload: response.data
+        });
+      },
+      err => {
+        console.log(UPDATE_BACK_OFFICE_SETTINGS_FAILURE, err);
+        dispatch({ type: UPDATE_BACK_OFFICE_SETTINGS_FAILURE });
       }
     );
   };
