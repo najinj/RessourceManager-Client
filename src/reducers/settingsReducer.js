bd80ceb70305b842daaf1e326f990fc703559f56
nnaji <nnaji@sqli.com>
2020-04-19 00:17:12 +0100

    Update store when settings are updated , Rename settings to settingsViewModel and add settingsModel that would be send to the server

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,14 +1,40 @@
 import {
   GET_BACK_OFFICE_SETTINGS_SUCCESS,
   GET_BACK_OFFICE_SETTINGS_REQUEST,
-  GET_BACK_OFFICE_SETTINGS_FAILURE
+  GET_BACK_OFFICE_SETTINGS_FAILURE,
+  UPDATE_BACK_OFFICE_SETTINGS_FAILURE,
+  UPDATE_BACK_OFFICE_SETTINGS_REQUEST,
+  UPDATE_BACK_OFFICE_SETTINGS_SUCCESS
 } from "../actions/settings-actions/types";
 
 const initalState = {
-  id: null,
-  emailSettings: null,
-  calendarSettings: null,
-  reservationSettings: null
+  settingsViewModel: {
+    id: null,
+    emailSettings: null,
+    calendarSettings: null,
+    reservationSettings: null
+  },
+  settingsModel: {
+    id: null,
+    emailSettings: null,
+    calendarSettings: null,
+    reservationSettings: null
+  }
+};
+
+const getSettingsModel = settings => {
+  return Object.keys(settings).reduce((acc, cur) => {
+    if (cur === "id") return { ...acc, id: settings.id };
+    return {
+      ...acc,
+      [cur]: settings[cur].reduce((ac, cu) => {
+        return {
+          ...ac,
+          [cu.name]: cu.value
+        };
+      }, {})
+    };
+  }, {});
 };
 
 const settingsReducer = (state = initalState, action) => {
@@ -19,13 +45,33 @@ const settingsReducer = (state = initalState, action) => {
     case GET_BACK_OFFICE_SETTINGS_SUCCESS:
       return {
         ...state,
-        id: action.payload.id,
-        emailSettings: action.payload.emailSettings,
-        calendarSettings: action.payload.calendarSettings,
-        reservationSettings: action.payload.reservationSettings
+        settingsViewModel: {
+          id: action.payload.id,
+          emailSettings: action.payload.emailSettings,
+          calendarSettings: action.payload.calendarSettings,
+          reservationSettings: action.payload.reservationSettings
+        },
+        settingsModel: getSettingsModel(action.payload)
       };
 
     case GET_BACK_OFFICE_SETTINGS_FAILURE:
+      return state;
+
+    case UPDATE_BACK_OFFICE_SETTINGS_REQUEST:
+      return state;
+
+    case UPDATE_BACK_OFFICE_SETTINGS_SUCCESS: {
+      return {
+        ...state,
+        settingsViewModel: {
+          id: action.payload.id,
+          emailSettings: action.payload.emailSettings,
+          calendarSettings: action.payload.calendarSettings,
+          reservationSettings: action.payload.reservationSettings
+        }
+      };
+    }
+    case UPDATE_BACK_OFFICE_SETTINGS_FAILURE:
       return state;
 
     default:
