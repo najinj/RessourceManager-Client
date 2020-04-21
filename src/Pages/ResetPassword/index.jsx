8878808f48138cf344f461449540e05cf0412f6d
nnaji <nnaji@sqli.com>
2020-04-21 16:04:41 +0000

    Add form for resetting the password

---
+++ src/Pages/ResetPassword/index.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import { Form, Icon, Input, Button } from "antd";
+import { shape, func } from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  resetPasswordRequest,
+  resetPassword
+} from "../../actions/auth-actions/actions";
+
+const mapDispatchToProps = dispatch => {
+  return {
+    requestPasswordRequest: email => dispatch(resetPasswordRequest(email)),
+    resetUserPassword: body => dispatch(resetPassword(body))
+  };
+};
+
+const ResetPasswordForm = ({
+  form,
+  resetUserPassword,
+  requestPasswordRequest
+}) => {
+  const [confirmDirty, setConfirmDirty] = useState(false);
+
+  const queryString = window.location.search;
+
+  const urlParams = new URLSearchParams(queryString);
+
+  const resetToken = urlParams.get("token");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        if (resetToken) {
+          const body = {
+            email: values.Email,
+            password: values.password,
+            confirmPassword: values.ConfirmPassword,
+            resetToken: resetToken.replace(/ /g, "+")
+          };
+          resetUserPassword(body);
+        } else requestPasswordRequest(values.Email);
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  const handleConfirmBlur = e => {
+    const { value } = e.target;
+    setConfirmDirty(confirmDirty || !!value);
+  };
+
+  const compareToFirstPassword = (rule, value, callback) => {
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+  const ValidatePasswordStrength = (rule, value, callback) => {
+    const regex = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/);
+    if (regex.test(value)) {
+      form.validateFields(["ConfirmPassword"], { force: true });
+      callback();
+    }
+    callback(
+      "Password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter"
+    );
+  };
+  const validateToNextPassword = (rule, value, callback) => {
+    ValidatePasswordStrength(rule, value, callback);
+    if (value && confirmDirty) {
+      form.validateFields(["ConfirmPassword"], { force: true });
+    }
+    callback();
+  };
+
+  const { getFieldDecorator } = form;
+  return (
+    <Form onSubmit={handleSubmit} className="login-form">
+      <Form.Item>
+        {getFieldDecorator("Email", {
+          rules: [{ required: true, message: "Please input your Email!" }]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Email"
+          />
+        )}
+      </Form.Item>
+      {resetToken ? (
+        <>
+          <Form.Item label="Password" hasFeedback>
+            {getFieldDecorator("password", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please input your password!"
+                },
+                {
+                  validator: validateToNextPassword
+                }
+              ]
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item label="Confirm Password" hasFeedback>
+            {getFieldDecorator("ConfirmPassword", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please confirm your password!"
+                },
+                {
+                  validator: compareToFirstPassword
+                }
+              ]
+            })(<Input.Password onBlur={handleConfirmBlur} />)}
+          </Form.Item>
+        </>
+      ) : null}
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Reset Password
+        </Button>
+        Or <Link to="/LogIn">Back To Login Page</Link>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const WrappedResetPasswordForm = Form.create({ name: "normal_login" })(
+  ResetPasswordForm
+);
+ResetPasswordForm.propTypes = {
+  form: shape(),
+  resetUserPassword: func,
+  requestPasswordRequest: func
+};
+ResetPasswordForm.defaultProps = {
+  form: {},
+  resetUserPassword: func,
+  requestPasswordRequest: func
+};
+
+const ConnectedWrappedResetPasswordForm = connect(
+  null,
+  mapDispatchToProps
+)(WrappedResetPasswordForm);
+
+export default ConnectedWrappedResetPasswordForm;
