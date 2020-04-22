5d8fbae17e3fa1b4b0ec7d2d6eace5e05442edba
nnaji <nnaji@sqli.com>
2020-04-22 22:27:00 +0000

    Add css for login page

--- src/Pages/LoginForm/index.jsx
+++ src/Pages/LoginForm/index.jsx
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { signIn, resetLoginErrors } from "../../actions/auth-actions/actions";
 
+import "./index.css";
+
 const mapDispatchToProps = dispatch => {
   return {
     logIn: user => dispatch(signIn(user)),
@@ -48,46 +50,53 @@ const NormalLoginForm = ({ form, logIn, errors, resetErrors }) => {
   };
   const { getFieldDecorator } = form;
   return (
-    <Form onSubmit={handleSubmit} className="login-form">
-      <Form.Item>
-        {getFieldDecorator("Email", {
-          rules: [{ required: true, message: "Please input your Email!" }]
-        })(
-          <Input
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Email"
-          />
-        )}
-      </Form.Item>
-      <Form.Item
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...(errors === 401 && {
-          help: "Bad Credentials",
-          validateStatus: "error"
-        })}
-      >
-        {getFieldDecorator("password", {
-          rules: [{ required: true, message: "Please input your Password!" }]
-        })(
-          <Input
-            onChange={resetError}
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-            name="password"
-          />
-        )}
-      </Form.Item>
-      <Form.Item>
-        <a className="login-form-forgot" href="/ResetPassword">
-          Forgot password
-        </a>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <Link to="/Signup">register now!</Link>
-      </Form.Item>
-    </Form>
+    <div className="form-container">
+      <div className="logo" />
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator("Email", {
+            rules: [{ required: true, message: "Please input your Email!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+            />
+          )}
+        </Form.Item>
+        <Form.Item
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...(errors === 401 && {
+            help: "Bad Credentials",
+            validateStatus: "error"
+          })}
+        >
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              onChange={resetError}
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <a className="login-form-forgot" href="/ResetPassword">
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <Link to="/Signup">register now!</Link>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
